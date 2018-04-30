26c0564f05db5c8a50f4db763ee42972f348ea3d
user11kahana <user11.kahana@gmail.com>
2018-05-01 00:53:47 +0300

    date range selector - more things

--- projectSchedule/ScheduleMe/Project1/src/app/components/dateRangeSelector.component.ts
+++ projectSchedule/ScheduleMe/Project1/src/app/components/dateRangeSelector.component.ts
@@ -4,28 +4,39 @@
     selector: "DateRangeSelector"
 })
 export class DateRangeSelectorComponent {
-    date: Date = new Date();
-    d: Date;
-    days: number;
+    currentDate: Date;
+    leftDay: Date;
+    rightDay: Date;
     sunday: number;
     saturday: number;
     currentMonth: number;
     constructor() {
-        this.sunday = this.GetDateOfSunday();
-        this.saturday = this.GetDateOfSaturday();
-        this.currentMonth = this.date.getMonth();
+        this.currentDate = new Date();
+        this.leftDay = new Date();
+        this.rightDay = new Date();
+        this.UpdateVariables();
+    }
+    UpdateVariables(): any {
+        this.sunday = this.GetLeftDay();
+        this.saturday = this.GetRightDay();
+        this.currentMonth = this.rightDay.getMonth() + 1;
+    }
+    DecreaseDate(): any {
+        this.currentDate.setDate(this.currentDate.getDate() - 7);
+        this.UpdateVariables();
+    }
+    IncreaseDate(): any {
+        this.currentDate.setDate(this.currentDate.getDate() + 7);
+        this.UpdateVariables();
     }
 
-    GetDateOfSunday(): number {
-        this.d = this.date;
-        this.days = this.d.getDay() - 1;
-        this.d = new Date(this.d.setDate(this.d.getDate() - this.days));
-        return this.d.getDay();
+    //???????לבדוק למה לא ממעדכן כראוי
+    GetLeftDay(): number {
+        this.leftDay.setDate(this.currentDate.getDate() - (this.currentDate.getDay() - 1));
+        return this.leftDay.getDate()-1;
     }
-    GetDateOfSaturday(): number {
-        this.d = this.date;
-        this.days = 7 - this.d.getDay();
-        this.d = new Date(this.d.setDate(this.d.getDate() + this.days));
-        return this.d.getDay();
+    GetRightDay(): number {
+        this.rightDay.setDate(this.currentDate.getDate() + (7 - this.currentDate.getDay()));
+        return this.rightDay.getDate()-1;
     }
 }
