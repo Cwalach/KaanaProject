d0a614111160ea5dc47a2bcc7c60cd6ac9627718
user4kahana <user4.kahana@gmail.com>
2018-05-25 10:55:01 +0300

    add nonActiveDayStateManager

---
+++ projectSchedule/ScheduleMe/Project1/src/app/Services/nonActiveDayStateManager.ts
@@ -0,0 +1,48 @@
+﻿import { Injectable } from "@angular/core"
+import "rxjs/add/operator/map"
+import { Observable } from "rxjs/Observable"
+import { NoActiveDay } from "../models/NoActiveDay"
+@Injectable()
+export class nonActiveDayStateManager {
+
+   private nonActiveDaysList: NoActiveDay[]; 
+   private activeDaysList: Array<number>;
+
+    GetChangeInListAddNoActiveDay(): Array<NoActiveDay>
+    {
+        return this.nonActiveDaysList;
+    }
+
+    GetChangeInListRemoveNoActiveDay(): Array<number> {
+        return this.activeDaysList;
+    }
+
+    //Add no Active day to the list
+    AddNoActiveDay(noActiveDay: NoActiveDay)
+    {
+        this.nonActiveDaysList.push(noActiveDay);
+    }
+
+    //Cancel no Active day that was adding
+    CencelNoActiveDay(noActiveDay: NoActiveDay) {
+        let index = this.nonActiveDaysList.indexOf(noActiveDay);
+        this.nonActiveDaysList.splice(index, 1);
+    }
+
+    //clear lists
+    ClearNoActiveDay() {
+        this.nonActiveDaysList.splice(0, this.nonActiveDaysList.length);
+        this.activeDaysList.splice(0, this.activeDaysList.length);
+    }
+
+    //is stil change
+    IsStillChanges(): boolean {
+        return this.nonActiveDaysList[0] == null && this.activeDaysList[0] == null;
+    }
+
+    //Remove from the db
+    RemoveNoActiveDay(noActiveDay: NoActiveDay)
+    {
+        this.activeDaysList.push(noActiveDay.Id);
+    }
+}
