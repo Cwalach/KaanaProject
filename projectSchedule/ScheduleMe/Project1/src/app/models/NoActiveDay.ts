317ea6935799e97ac6c800ea5af914db759249bd
user4kahana <user4.kahana@gmail.com>
2018-05-25 11:39:48 +0300

    add NoActiveDay ts

---
+++ projectSchedule/ScheduleMe/Project1/src/app/models/NoActiveDay.ts
@@ -0,0 +1,16 @@
+﻿
+export class NoActiveDay {
+
+    public Id: number;
+    public Date: Date;
+    public OrderNumber: number;
+    public Reason: string;
+    
+    constructor(id, date, nameOrderNumber, reason) {
+        this.Id = id;
+        this.Date = date;
+        this.OrderNumber = nameOrderNumber;
+        this.Reason = reason;
+
+    }
+}
